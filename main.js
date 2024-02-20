33b2457d0f58866ee56dc35c3edd22beaa6cdff5
codaengii2 <carla7039@naver.com>
2024-02-20 19:09:32 +0900

    root 설정

--- main.js
+++ main.js
@@ -23,9 +23,10 @@
 // getLatestNews();
 
 const getNews = async () => {
-  const url = new URL(
-    `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`
-  );
+  // const url = new URL(
+  //   `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`
+  // );
+  const url = new URL(`http://https://noona-news.netlify.app//top-headlines`);
   const response = await fetch(url);
   const data = await response.json();
   console.log(data);
