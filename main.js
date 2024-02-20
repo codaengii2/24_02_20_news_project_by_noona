e4c3344df471c78627e27c7057a5c7f8a64e7f37
codaengii2 <carla7039@naver.com>
2024-02-20 19:20:26 +0900

    url 수정

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ const getNews = async () => {
   // const url = new URL(
   //   `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`
   // );
-  const url = new URL(`http://noona-news.netlify.app/top-headlines`);
+  const url = new URL(`https://noona-news.netlify.app/top-headlines`);
   const response = await fetch(url);
   const data = await response.json();
   console.log(data);
