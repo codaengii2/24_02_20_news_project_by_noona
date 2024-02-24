0b81521bbf928c9e3c23ccd9823f4b29d62ebaa0
codaengii2 <carla7039@naver.com>
2024-02-24 21:33:30 +0900

    pagination setParams

--- main.js
+++ main.js
@@ -69,8 +69,11 @@ searchInput.addEventListener("keydown", (event) => {
 
 const addNewsRender = async () => {
   try {
+    url.searchParams.set("page", page); // => &page=page
+    url.searchParams.set("pageSize", pageSize); // =>&pageSize=pageSize
+    //url 호출하기 전 추가할 params
     const response = await fetch(url);
-    console.log(response);
+    // console.log(response);
     const data = await response.json();
     console.log(data);
     if (response.status === 200) {
@@ -163,26 +166,41 @@ const paginationRender = () => {
   //page
   //pageSize
   //groupSize
+  //totalGroup
+  const totalPages = Math.ceil(totalResults / pageSize);
 
   //pageGroup
   const pageGroup = Math.ceil(page / groupSize);
 
   //lastPage
   const lastPage = pageGroup * groupSize;
+  //마지막 페이지그룹이 그룹사이즈보다 작을경우 lastPage = tatalPage
+  if (lastPage > totalPages) {
+    lastPage = totalPages;
+  }
 
   //firstPage
-  const firstPage = lastPage - (groupSize - 1);
+  const firstPage =
+    lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
 
   let paginationHTML = ``;
 
   for (let i = firstPage; i <= lastPage; i++) {
-    paginationHTML += `<li class="page-item"><a class="page-link" href="#">${i}</a></li>`;
+    paginationHTML += `<li class="page-item ${
+      i === page ? "active" : ""
+    }" onClick="moveToPage(${i})"><a class="page-link" href="#">${i}</a></li>`;
   }
 
   document.querySelector(".pagination").innerHTML = paginationHTML;
 };
-getNews();
 
+const moveToPage = (pageNum) => {
+  console.log("moveToPage", pageNum);
+  page = pageNum;
+  addNewsRender();
+};
+
+getNews();
 //1. 버튼들 클릭이벤트
 //2. 카테고리별 뉴스 가져오기
 //3. 뉴스 보여주기
